f2a0c8389f6adf14ac047412f25732482c9c347f
Zak Brindle <z4kbrindle@gmail.com>
2026-05-31 14:00:25 +0100

    Android Wrapper

---
+++ herd-search-vite/android/app/src/main/java/com/herdsearch/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.herdsearch.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ herd-search-vite/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.herdsearch.app',
+    appName: 'Herd Search',
+    webDir: 'dist'
+};
+
+export default config;
